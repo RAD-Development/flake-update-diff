fd9fbf1ad7a27098971903271df018abb35c43cd
ahuston-0 <aliceghuston@gmail.com>
2024-07-28 15:30:18 -0400

    add cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,24 @@
 {
   description = "Application packaged using poetry2nix";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/?priority=1&want-mass-query=true"
+      "https://attic.alicehuston.xyz/cache-nix-dot?priority=4&want-mass-query=true"
+      "https://nix-community.cachix.org/?priority=10&want-mass-query=true"
+    ];
+    trusted-substituters = [
+      "https://cache.nixos.org"
+      "https://attic.alicehuston.xyz/cache-nix-dot"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "cache-nix-dot:Od9KN34LXc6Lu7y1ozzV1kIXZa8coClozgth/SYE7dU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
